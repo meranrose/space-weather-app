8980d678b445a4773277f1c3a22f7a64cdb4b2c8
Meran <meranrose@gmail.com>
2013-07-31 19:20:51 -0700

    Create ViewController.h

---
+++ ViewController.h
@@ -0,0 +1,22 @@
+//
+//  ViewController.h
+//  UIWebView
+//
+//  Created by meran on 7/29/13.
+//  Copyright (c) 2013 meran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+{
+    IBOutlet UIWebView *myWebView;
+    IBOutlet UIBarButtonItem *p1;
+    IBOutlet UIBarButtonItem *p2;
+    IBOutlet UIBarButtonItem *p3;
+}
+
+- (IBAction)p1:(id)sender;
+
+@end
